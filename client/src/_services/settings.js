b7708c926f3eb5c724b2900b01556b20ee1ed252
tzymni <tomasz.zymni@gmail.com>
2020-04-12 22:46:39 +0200

    Merged cottages. Version bump.

--- client/src/_services/settings.js
+++ client/src/_services/settings.js
@@ -4,7 +4,7 @@ export const Settings = {
 };
 
 // TODO add global variable with main API url
-const systemVersion = '0.9.0';
+const systemVersion = '0.10.0';
 
 function getSystemVersion() {
     return systemVersion;
