a181088570838e13868570eb48c085d5934e74b5
tzymni <tomasz.zymni@gmail.com>
2021-04-21 22:18:13 +0200

    Added config.js to gitignore. Refs #12.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,49 +1,48 @@
 <template>
-    <div id="app">
-        <component v-bind:is="component"/>
-    </div>
+  <div id="app">
+    <component v-bind:is="component"/>
+  </div>
 </template>
 <script>
-    import Menu from './components/Menu.vue'
-    import LoginPanel from "./components/LoginPanel";
+import Menu from './components/Menu.vue'
+import LoginPanel from "./components/LoginPanel";
 
-    require('@/assets/css/global.css');
+require('@/assets/css/global.css');
 
-
-    export default {
-        data() {
-            return {
-                component: "LoginPanel",
-            }
-        },
-        created() {
-
-            const loggedIn = sessionStorage.getItem('token');
-            if (loggedIn) {
-                this.component = Menu;
-            }
-        },
-        name: 'app',
-        components: {
-            Menu,
-            LoginPanel,
-        },
-        methods: {
-            toggleComponent() {
-                if (this.component === Menu) {
-                    this.component = LoginPanel;
-                } else {
-                    this.component = Menu;
-                }
-            },
-        },
-        install(Vue) {
-            Vue.component('menu', Menu)
-
-        }
+export default {
+  data() {
+    return {
+      component: "LoginPanel",
     }
-    export {Menu}
+  },
+  created() {
+
+    const loggedIn = sessionStorage.getItem('token');
+    if (loggedIn) {
+      this.component = Menu;
+    }
+  },
+  name: 'app',
+  components: {
+    Menu,
+    LoginPanel,
+  },
+  methods: {
+    toggleComponent() {
+      if (this.component === Menu) {
+        this.component = LoginPanel;
+      } else {
+        this.component = Menu;
+      }
+    },
+  },
+  install(Vue) {
+    Vue.component('menu', Menu)
+
+  }
+}
+export {Menu}
 </script>
 <style>
-    @import './assets/css/global.css';
+@import './assets/css/global.css';
 </style>
