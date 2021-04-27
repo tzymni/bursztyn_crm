c95a2add45487cf343f03825216eab013ddccc18
tzymni <tomasz.zymni@gmail.com>
2021-04-27 19:23:10 +0200

    Created a new config variable to store gantt-schedule-timeline-calendar API key. Refs #22.

--- client/src/components/TimelineCalendar.vue
+++ client/src/components/TimelineCalendar.vue
@@ -69,6 +69,7 @@ import {cleaningEventServices} from "@/_services/cleaning_event_service";
 import ReservationForm from "@/components/ReservationForm";
 import CleaningForm from "@/components/CleaningForm";
 import CheckAvaliabilityForm from "./CheckAvailabilityForm";
+import {config} from "@/config";
 
 let gstc, state;
 
@@ -86,18 +87,18 @@ export default {
       state: null,
       cottages: {},
       form: {
-        option: 'RESERVATION',
+        option: config.event.reservationType,
       },
       eventTypes: [
-        {text: 'Tylko rezerwacje', value: 'RESERVATION'},
-        {text: 'Tylko zmiany', value: 'CLEANING'}
+        {text: 'Tylko rezerwacje', value: config.event.reservationType},
+        {text: 'Tylko zmiany', value: config.event.cleaningType}
       ],
       editId: null,
       config: {
         actions: {
           'chart-timeline-items-row-item': [this.clickAction],
         },
-        licenseKey: '====BEGIN LICENSE KEY====\nXOfH/lnVASM6et4Co473t9jPIvhmQ/l0X3Ewog30VudX6GVkOB0n3oDx42NtADJ8HjYrhfXKSNu5EMRb5KzCLvMt/pu7xugjbvpyI1glE7Ha6E5VZwRpb4AC8T1KBF67FKAgaI7YFeOtPFROSCKrW5la38jbE5fo+q2N6wAfEti8la2ie6/7U2V+SdJPqkm/mLY/JBHdvDHoUduwe4zgqBUYLTNUgX6aKdlhpZPuHfj2SMeB/tcTJfH48rN1mgGkNkAT9ovROwI7ReLrdlHrHmJ1UwZZnAfxAC3ftIjgTEHsd/f+JrjW6t+kL6Ef1tT1eQ2DPFLJlhluTD91AsZMUg==||U2FsdGVkX1/SWWqU9YmxtM0T6Nm5mClKwqTaoF9wgZd9rNw2xs4hnY8Ilv8DZtFyNt92xym3eB6WA605N5llLm0D68EQtU9ci1rTEDopZ1ODzcqtTVSoFEloNPFSfW6LTIC9+2LSVBeeHXoLEQiLYHWihHu10Xll3KsH9iBObDACDm1PT7IV4uWvNpNeuKJc\npY3C5SG+3sHRX1aeMnHlKLhaIsOdw2IexjvMqocVpfRpX4wnsabNA0VJ3k95zUPS3vTtSegeDhwbl6j+/FZcGk9i+gAy6LuetlKuARjPYn2LH5Be3Ah+ggSBPlxf3JW9rtWNdUoFByHTcFlhzlU9HnpnBUrgcVMhCQ7SAjN9h2NMGmCr10Rn4OE0WtelNqYVig7KmENaPvFT+k2I0cYZ4KWwxxsQNKbjEAxJxrzK4HkaczCvyQbzj4Ppxx/0q+Cns44OeyWcwYD/vSaJm4Kptwpr+L4y5BoSO/WeqhSUQQ85nvOhtE0pSH/ZXYo3pqjPdQRfNm6NFeBl2lwTmZUEuw==\n====END LICENSE KEY====',
+        licenseKey: config.ganttScheduleTimelineCalendarKey,
         plugins: [TimelinePointer(), Selection(), HighlightWeekends({
           weekdays: [6, 0],
           className: "highlight-weekend"
@@ -187,7 +188,7 @@ export default {
       function showModal() {
 
         self.editId = data.item.event_id
-        if (data.item.type === 'RESERVATION') {
+        if (data.item.type === config.event.reservationType) {
           self.$bvModal.show("reservation-form-modal")
         } else {
           self.$bvModal.show("cleaning-form-modal")
@@ -215,7 +216,7 @@ export default {
     filterCalendarEvents(selected) {
       let type = selected.currentValue.value
 
-      if (type === 'RESERVATION') {
+      if (type === config.event.reservationType) {
         this.setReservations()
       } else {
         this.setCleaningEvents()
@@ -257,8 +258,8 @@ export default {
 
     },
     setReservations() {
-      var self = this;
-      reservationService.getEvents('RESERVATION').then(function (response) {
+      const self = this;
+      reservationService.getEvents(config.event.reservationType).then(function (response) {
 
             let list = []
             const itemsNew = {}
--- client/src/config_example.js
+++ client/src/config_example.js
@@ -4,9 +4,11 @@ export const config = {
         path: '',
     },
     event: {
-      reservationType: 'RESERVATION',
-      cleaningType: 'CLEANING',
-      allType: 'ALL'
+        reservationType: 'RESERVATION',
+        cleaningType: 'CLEANING',
+        allType: 'ALL'
     },
-    systemVersion: '0.20.3'
+    systemVersion: '0.20.3',
+    // this is a trial key, to generate own key please visit https://gstc.neuronet.io/free-key/
+    ganttScheduleTimelineCalendarKey: '====BEGIN LICENSE KEY====XOfH/lnVASM6et4Co473t9jPIvhmQ/l0X3Ewog30VudX6GVkOB0n3oDx42NtADJ8HjYrhfXKSNu5EMRb5KzCLvMt/pu7xugjbvpyI1glE7Ha6E5VZwRpb4AC8T1KBF67FKAgaI7YFeOtPFROSCKrW5la38jbE5fo+q2N6wAfEti8la2ie6/7U2V+SdJPqkm/mLY/JBHdvDHoUduwe4zgqBUYLTNUgX6aKdlhpZPuHfj2SMeB/tcTJfH48rN1mgGkNkAT9ovROwI7ReLrdlHrHmJ1UwZZnAfxAC3ftIjgTEHsd/f+JrjW6t+kL6Ef1tT1eQ2DPFLJlhluTD91AsZMUg==||U2FsdGVkX1/SWWqU9YmxtM0T6Nm5mClKwqTaoF9wgZd9rNw2xs4hnY8Ilv8DZtFyNt92xym3eB6WA605N5llLm0D68EQtU9ci1rTEDopZ1ODzcqtTVSoFEloNPFSfW6LTIC9+2LSVBeeHXoLEQiLYHWihHu10Xll3KsH9iBObDACDm1PT7IV4uWvNpNeuKJcpY3C5SG+3sHRX1aeMnHlKLhaIsOdw2IexjvMqocVpfRpX4wnsabNA0VJ3k95zUPS3vTtSegeDhwbl6j+/FZcGk9i+gAy6LuetlKuARjPYn2LH5Be3Ah+ggSBPlxf3JW9rtWNdUoFByHTcFlhzlU9HnpnBUrgcVMhCQ7SAjN9h2NMGmCr10Rn4OE0WtelNqYVig7KmENaPvFT+k2I0cYZ4KWwxxsQNKbjEAxJxrzK4HkaczCvyQbzj4Ppxx/0q+Cns44OeyWcwYD/vSaJm4Kptwpr+L4y5BoSO/WeqhSUQQ85nvOhtE0pSH/ZXYo3pqjPdQRfNm6NFeBl2lwTmZUEuw======END LICENSE KEY===='
 }
